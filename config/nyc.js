696bbb19559fd0deb5c3c029b1f67db2c887faf7
Aliza Aufrichtig <alizauf@users.noreply.github.com>
2018-09-14 19:16:31 -0400

    Adding styles to link and adding concept of bike name

--- config/nyc.js
+++ config/nyc.js
@@ -4,8 +4,10 @@ module.exports = {
   MAP_CENTER: "[40.7055585, -73.989109 ]",
   MAP_ZOOM: 13,
   CITY_NAME: 'DC',
-  BIKESHARE_NAME: 'Citi Bikes',
+  BIKESHARE_NAME: 'Citi Bike',
   BIKESHARE_NAME_PLURAL: 'Citi Bikes',
+  BIKESHARE_BIKE_NAME: 'Citi Bike',
+  BIKESHARE_BIKE_NAME_PLURAL: 'Citi Bikes',
   BIKESHARE_DATA_URL: 'https://www.citibikenyc.com/system-data',
-  AUTHOR: "<a href='https://twitter.com/alizauf'>Aliza Aufrichtig</a>",
+  AUTHOR: "<a style='color: #0498e4; text-decoration: none;' href='https://twitter.com/alizauf'>Aliza Aufrichtig</a>",
 }
