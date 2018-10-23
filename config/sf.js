63a308661d67141fdbec33612d3cb68a12a60bf0
Daniel Schep <dschep@gmail.com>
2018-10-23 17:44:36 -0400

    Ford GoBike config for SF!

---
+++ config/sf.js
@@ -0,0 +1,13 @@
+module.exports = {
+  STATION_STATUS_GBFS: "'https://gbfs.fordgobike.com/gbfs/en/station_status.json'",
+  STATION_INFORMATION_GBFS: "'https://gbfs.fordgobike.com/gbfs/en/station_information.json'",
+  MAP_CENTER: "[ 37.7786982,-122.4152072 ]",
+  MAP_ZOOM: 13,
+  CITY_NAME: 'San Francisco',
+  BIKESHARE_NAME: 'Ford GoBike',
+  BIKESHARE_NAME_PLURAL: 'Ford GoBikes',
+  BIKESHARE_BIKE_NAME: 'Ford GoBike',
+  BIKESHARE_BIKE_NAME_PLURAL: 'Ford GoBikes',
+  BIKESHARE_DATA_URL: 'https://www.fordgobike.com/system-data',
+  AUTHOR: "<a style='color: #0498e4; text-decoration: none;' href='https://twitter.com/alizauf'>Aliza Aufrichtig</a>, and remixed by <a style='color: #0498e4; text-decoration: none;' href='https://twitter.com/schep_'>Daniel Schep</a> for SF. See original at <a style='color: #0498e4; text-decoration: none;' href='https://www.i-want-to-ride-an-electric-citi.bike'>i-want-to-ride-an-electric-citi.bike",
+}
